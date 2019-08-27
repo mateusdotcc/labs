233ad3c88ef6cb98fd7f75a72200c523e6a456e3
Mateus Henrique <msnmateus@live.com>
2019-08-27 11:37:12 -0300

    feature: Redis queue

---
+++ nodejs/gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '25ca601cdc9e67',
+    pass: 'd45b80ed5ace00',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
