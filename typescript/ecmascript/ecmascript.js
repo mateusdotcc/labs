4795e57625ea142086774ce353b5cc8dc43648af
Mateus Henrique <msnmateus@live.com>
2019-12-30 17:02:32 -0300

    chore: udpate typescript

--- typescript/ecmascript/ecmascript.js
+++ typescript/ecmascript/ecmascript.js
@@ -4,6 +4,14 @@
 // console.log(isCan);
 // isCan = "?"; // hoisting
 // let isCan = "?"
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var _this = this;
 var isCan = "?";
 console.log(isCan);
 var isCold = true;
@@ -15,3 +23,100 @@ if (isCold) {
 var cpf = "123.456.000-99";
 // cpf = "888.999.123-00";
 console.log(cpf);
+var sumOpt = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(sumOpt(2, 2));
+var sub = function (n1, n2) { return n1 - n2; };
+console.log(sub(2, 3));
+var hello = function () { return console.log("Hello!"); };
+console.log(hello);
+var talkTo = function (people) { return console.log("Hello" + people); };
+talkTo("John");
+// this
+function normalWithThis() {
+    console.log(this);
+}
+var normalWithThisSpecial = normalWithThis.bind("abc");
+normalWithThis();
+// this??? r => Window
+console.log(this);
+var arrowWithThis = function () { return console.log(_this); }; // r => Window
+arrowWithThis();
+var arrowWithThisSpecial = arrowWithThis.bind({ name: "Ana" });
+arrowWithThisSpecial();
+// Params
+function count(init, end) {
+    if (init === void 0) { init = 5; }
+    if (end === void 0) { end = init - 5; }
+    console.log(init);
+    while (init > end) {
+        init--;
+        console.log(init);
+    }
+    console.log("end");
+}
+count();
+count(3);
+// Spread & Rest
+var numbers = [1, 10, 99, -5, 200, 1034];
+console.log(Math.max.apply(Math, numbers));
+// Spread
+var teamA = ["João", "Maria", "Fernanda"];
+var teamB = __spreadArrays(["Fernando", "Miguel"], teamA, ["Lorena"]);
+console.log(teamB);
+// Rest
+function returnArr(a) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    console.log(a); // => 1
+    return args; // => rest 2, 3 +
+}
+var numbersOpt = returnArr(1, 2, 3, 4, 5, 6);
+console.log(numbersOpt);
+console.log(returnArr.apply(void 0, numbers)); // Spread
+// Rest & Spread (Tupla)
+var tupla = [1, "abc", false];
+function tuplaParam1(a, b, c) {
+    console.log("1) " + a + " " + b + " " + c);
+}
+tuplaParam1.apply(void 0, tupla);
+function tuplaParam2() {
+    var params = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        params[_i] = arguments[_i];
+    }
+    console.log(Array.isArray(params));
+    console.log("2) " + params[0] + " " + params[1] + " " + params[2]);
+}
+tuplaParam2.apply(void 0, tupla);
+// Destructuring (array)
+var carac = ["Motor Zetec 1.8", 2020];
+// const motor = carac[0]
+// const year = carac[1];
+var motor = carac[0], yyear = carac[1];
+console.log(motor);
+console.log(yyear);
+var _a = [2, 3], w = _a[0], z = _a[1];
+// Destructuring (object)
+var item = {
+    name: "SSD 480GB",
+    price: 200,
+    carac: {
+        ww: "Imported"
+    }
+};
+var nameItem = item.name;
+var priceItem = item.price;
+var nn = item.name, p = item.price, ww = item.carac.ww; // name: alias
+console.log(nn, p, ww);
+// Template String
+var userID = "Suporte";
+var notifications = "9";
+// const welcome = "Welcome" + userID + "Notifications" + notifications;
+var welcome = "Welcome " + userID + " Notifications " + (parseInt(notifications) > 9 ? "+9" : notifications);
+console.log(welcome);
+console.log("" + (1 + 1) * 30);
+console.log("" + carac[0]);
--- typescript/ecmascript/ecmascript.ts
+++ typescript/ecmascript/ecmascript.ts
@@ -18,3 +18,117 @@ if (isCold) {
 const cpf: string = "123.456.000-99";
 // cpf = "888.999.123-00";
 console.log(cpf);
+
+const sumOpt = function(n1: number, n2: number): number {
+  return n1 + n2;
+};
+console.log(sumOpt(2, 2));
+
+const sub = (n1: number, n2: number) => n1 - n2;
+console.log(sub(2, 3));
+
+const hello = () => console.log("Hello!");
+console.log(hello);
+
+const talkTo = (people: string) => console.log("Hello" + people);
+talkTo("John");
+
+// this
+
+function normalWithThis() {
+  console.log(this);
+}
+
+const normalWithThisSpecial = normalWithThis.bind("abc");
+normalWithThis();
+
+// this??? r => Window
+console.log(this);
+const arrowWithThis = () => console.log(this); // r => Window
+arrowWithThis();
+
+const arrowWithThisSpecial = arrowWithThis.bind({ name: "Ana" });
+arrowWithThisSpecial();
+
+// Params
+
+function count(init: number = 5, end: number = init - 5): void {
+  console.log(init);
+  while (init > end) {
+    init--;
+    console.log(init);
+  }
+  console.log("end");
+}
+count();
+count(3);
+
+// Spread & Rest
+
+const numbers = [1, 10, 99, -5, 200, 1034];
+console.log(Math.max(...numbers));
+
+// Spread
+const teamA: string[] = ["João", "Maria", "Fernanda"];
+const teamB: string[] = ["Fernando", "Miguel", ...teamA, "Lorena"];
+console.log(teamB);
+
+// Rest
+function returnArr(a: number, ...args: number[]): number[] {
+  console.log(a); // => 1
+  return args; // => rest 2, 3 +
+}
+const numbersOpt = returnArr(1, 2, 3, 4, 5, 6);
+console.log(numbersOpt);
+console.log(returnArr(...numbers)); // Spread
+
+// Rest & Spread (Tupla)
+const tupla: [number, string, boolean] = [1, "abc", false];
+
+function tuplaParam1(a: number, b: string, c: boolean): void {
+  console.log(`1) ${a} ${b} ${c}`);
+}
+tuplaParam1(...tupla);
+
+function tuplaParam2(...params: [number, string, boolean]) {
+  console.log(Array.isArray(params));
+  console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
+}
+tuplaParam2(...tupla);
+
+// Destructuring (array)
+const carac = ["Motor Zetec 1.8", 2020];
+// const motor = carac[0]
+// const year = carac[1];
+const [motor, yyear] = carac;
+console.log(motor);
+console.log(yyear);
+const [w, z] = [2, 3];
+
+// Destructuring (object)
+const item = {
+  name: "SSD 480GB",
+  price: 200,
+  carac: {
+    ww: "Imported"
+  }
+};
+const nameItem = item.name;
+const priceItem = item.price;
+const {
+  name: nn,
+  price: p,
+  carac: { ww }
+} = item; // name: alias
+console.log(nn, p, ww);
+
+// Template String
+const userID: string = "Suporte";
+const notifications: string = "9";
+// const welcome = "Welcome" + userID + "Notifications" + notifications;
+const welcome = `Welcome ${userID} Notifications ${
+  parseInt(notifications) > 9 ? "+9" : notifications
+}`;
+console.log(welcome);
+console.log(`${(1 + 1) * 30}`);
+console.log(`${carac[0]}`);
